3caee9ad8b19ede3217e5989aa5273feab9cce0b
eshockx <eshock@163.com>
2019-05-25 17:50:23 +0800

    add main/home/commit/repo component

--- src/main/angular-antd/src/app/app-routing.module.ts
+++ src/main/angular-antd/src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainComponent } from './component/main/main.component';
+import { HomeComponent } from './component/home/home.component';
+import { CommitComponent } from './component/commit/commit.component';
+import { RepoComponent } from './component/repo/repo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/main/home', pathMatch: 'full' },
+  { path: 'main', component: MainComponent, children: [
+    { path: 'home', component: HomeComponent },
+    { path: 'commit', component: CommitComponent },
+    { path: 'repo', component: RepoComponent }
+  ] }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/main/angular-antd/src/app/app.module.ts
+++ src/main/angular-antd/src/app/app.module.ts
@@ -1,18 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { NgZorroAntdModule, NZ_I18N, en_US } from 'ng-zorro-antd';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './component/main/main.component';
+import { HomeComponent } from './component/home/home.component';
+import { CommitComponent } from './component/commit/commit.component';
+import { RepoComponent } from './component/repo/repo.component';
+
+registerLocaleData(en);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    HomeComponent,
+    CommitComponent,
+    RepoComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule,
+    NgZorroAntdModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
