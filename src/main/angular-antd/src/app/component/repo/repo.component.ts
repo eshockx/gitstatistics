b301129f13b4b3fc354997b9d22a79aa4e200012
eshockx <eshock@163.com>
2019-05-27 22:53:37 +0800

    update repo

--- src/main/angular-antd/src/app/component/repo/repo.component.ts
+++ src/main/angular-antd/src/app/component/repo/repo.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+import { Repo } from '../../domain/repo';
 
 @Component({
   selector: 'app-repo',
@@ -7,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RepoComponent implements OnInit {
 
-  constructor() { }
+  repos: Repo[] = [];
+
+  isLoading: boolean[] = [];
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.http.get<Repo[]>(environment.baseUrl + '/repo/find/all').subscribe(data => {
+      this.repos = data;
+      this.isLoading = new Array(this.repos.length);
+    });
   }
 
+  extractFromRepo(repo: Repo, index: number) {
+    this.isLoading[index] = true;
+    this.http.get(environment.baseUrl + '/commit/extract/repo/' + repo.id).subscribe(data => this.isLoading[index] = false);
+  }
 }
